302b3d5e1f5aad7fa622643b1395e3b2e953b7d2
George Spatacean <george.spatacean@gmail.com>
2021-09-02 10:02:29 +0300

    Executable jar

---
+++ src/main/java/net/gspatace/json/schema/podo/generator/JsonSchemaPodoGenerator.java
@@ -0,0 +1,7 @@
+package net.gspatace.json.schema.podo.generator;
+
+public class JsonSchemaPodoGenerator {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
