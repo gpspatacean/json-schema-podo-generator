0dc094991573168c3c0b6a1a9b26a2d7b371e150
George Spatacean <george.spatacean@gmail.com>
2021-12-01 23:41:38 +0200

    Add basic run tests for List and CustomConfigHelper commands.

---
+++ src/test/java/net/gspatace/json/schema/podo/generator/cli/ListCommandTests.java
@@ -0,0 +1,20 @@
+package net.gspatace.json.schema.podo.generator.cli;
+
+import net.gspatace.json.schema.podo.generator.cli.commands.ListCommand;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Simple run of the {@link ListCommand} command
+ *
+ * @author George Spătăcean
+ */
+public class ListCommandTests {
+
+    @Test
+    public void runListCommand(){
+        new ListCommand().run();
+        assertTrue(true);
+    }
+}
