5a09c352d9b2ef7e2c2f048bf5e25a9d97b37f34
George Spatacean <george.spatacean@gmail.com>
2021-12-02 10:54:57 +0200

    Add unit tests for GeneratorService and for GenerateCommand

--- src/test/java/net/gspatace/json/schema/podo/generator/langs/TestGenerator.java
+++ src/test/java/net/gspatace/json/schema/podo/generator/langs/TestGenerator.java
@@ -4,22 +4,27 @@ import net.gspatace.json.schema.podo.generator.annotations.CustomProperties;
 import net.gspatace.json.schema.podo.generator.annotations.SchemaGenerator;
 import net.gspatace.json.schema.podo.generator.base.AbstractGenerator;
 import net.gspatace.json.schema.podo.generator.base.BaseOptions;
+import net.gspatace.json.schema.podo.generator.templating.TemplateFile;
 import picocli.CommandLine;
 
 /**
  * Basic Generator used as Test Bed for unit tests
- * 
+ *
  * @author George Spătăcean
  */
-@SchemaGenerator(name = "test-generator", description = "Generator use just for unit testing", embeddedResourceLocation = "testgen")
+@SchemaGenerator(name = "test-generator", description = "Generator use just for unit testing", embeddedResourceLocation = "templates")
 public class TestGenerator extends AbstractGenerator {
 
-    public TestGenerator(final BaseOptions baseOptions){
+    private final TestGeneratorCustomProperties customProperties = new TestGeneratorCustomProperties();
+
+    public TestGenerator(final BaseOptions baseOptions) {
         super(baseOptions);
+        addTemplateFile(new TemplateFile("testGenerator.mustache", "test"));
+        parseCustomOptionsProperties(customProperties);
     }
 
     @CustomProperties
-    public static class TestGeneratorCustomProperties{
+    public static class TestGeneratorCustomProperties {
         @CommandLine.Option(names = "-customOptionOne", description = "Custom Option One")
         private String optionOne;
 
