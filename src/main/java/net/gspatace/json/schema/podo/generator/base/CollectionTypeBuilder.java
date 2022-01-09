0c87ee9443066e75ca7f1a6bc8592a6863cd38d4
George Spatacean <george.spatacean@gmail.com>
2022-01-09 11:56:15 +0200

    javadoc fixups

--- src/main/java/net/gspatace/json/schema/podo/generator/base/CollectionTypeBuilder.java
+++ src/main/java/net/gspatace/json/schema/podo/generator/base/CollectionTypeBuilder.java
@@ -11,8 +11,8 @@ public interface CollectionTypeBuilder {
      * For a given member, if it is a collection, return the language specific construct.
      * This can be C++ templates, Java/C# generics, etc.
      *
-     * For instance, for C++, the representation of a collection of "Product"s, could translate to
-     * @<code> std::vector<Product> products{};</code>
+     * For instance, for C++, the representation of a collection of "Product"s, would translate to
+     * {@code std::vector<Product> products{};}
      *
      * @param memberVariableData the property that is a collection
      * @return String containing the language specific construct.
