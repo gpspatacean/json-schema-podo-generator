ea10e5416cab888a000632d0f482b83e2582e0cc
George Spatacean <george.spatacean@gmail.com>
2022-08-01 00:10:08 +0300

    Add archive builder It takes a list of processed source files and creates a ZIP archive

--- modules/core/src/main/java/net/gspatace/json/schema/podo/generator/core/base/ProcessedSourceFile.java
+++ modules/core/src/main/java/net/gspatace/json/schema/podo/generator/core/base/ProcessedSourceFile.java
@@ -1,14 +1,18 @@
 package net.gspatace.json.schema.podo.generator.core.base;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 /**
+ * Container of a processed source file
+ *
  * @author George Spătăcean
  */
 
 @AllArgsConstructor
 @Getter
+@EqualsAndHashCode
 public class ProcessedSourceFile {
     /**
      * File path including directories from the
