dad2d965bf305856ba2cbe3e62f313ced37e9f20
George Spatacean <george.spatacean@gmail.com>
2023-01-30 23:05:29 +0200

    Add unit tests for all /generators controller APIs

--- modules/rest-api/src/test/java/net/gspatace/json/schema/podo/generator/rest/GeneratorControllerTests.java
+++ modules/rest-api/src/test/java/net/gspatace/json/schema/podo/generator/rest/GeneratorControllerTests.java
@@ -1,17 +1,31 @@
 package net.gspatace.json.schema.podo.generator.rest;
 
+import net.gspatace.json.schema.podo.generator.core.services.GeneratorDescription;
+import net.gspatace.json.schema.podo.generator.core.services.OptionDescription;
 import net.gspatace.json.schema.podo.generator.rest.controllers.GeneratorController;
-
 import net.gspatace.json.schema.podo.generator.rest.services.GeneratorsService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(GeneratorController.class)
 class GeneratorControllerTests {
@@ -24,6 +38,74 @@ class GeneratorControllerTests {
 
     @Test
     void getAvailableGenerators() throws Exception {
-        mockMvc.perform(get("/generators")).andDo(print()).andExpect(status().isOk());
+        final GeneratorDescription generator = GeneratorDescription.builder()
+                .name("test-generator")
+                .description("test-generator description")
+                .build();
+
+        final List<GeneratorDescription> mockedResult = Collections.singletonList(generator);
+        when(service.listGenerators()).thenReturn(mockedResult);
+        mockMvc.perform(get("/generators"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].name", is(generator.getName())));
+    }
+
+    @Test
+    void getSpecificGeneratorOptions() throws Exception {
+        final OptionDescription optionDescription = OptionDescription
+                .builder()
+                .name("--option-name")
+                .description("--option-name description")
+                .defaultValue("--option-name defaultValue")
+                .build();
+
+        final Set<OptionDescription> mockedResult = Collections.singleton(optionDescription);
+        when(service.listGeneratorProperties("test-generator")).thenReturn(mockedResult);
+        mockMvc.perform(get("/generators/test-generator"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].name", is(optionDescription.getName())));
+    }
+
+    @Test
+    void getDummyResourceUsingJsonPayload() throws Exception {
+        final byte[] resourceContents = new byte[]{'a', 'b', 'c'};
+        final InputStreamResource mockResource = new InputStreamResource(new ByteArrayInputStream(resourceContents));
+        when(service.buildCodeArchive(anyString(), anyString(), anyString())).thenReturn(mockResource);
+
+        mockMvc.perform(
+                        post("/generators/test-generator")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("{\"dummy\":\"schema\"}")
+                                .queryParam("options", "[]")
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_OCTET_STREAM))
+                .andExpect(content().bytes(resourceContents));
+    }
+
+    @Test
+    void getDummyResourceUsingMultiPartFormPayload() throws Exception {
+        final byte[] resourceContents = new byte[]{'a', 'b', 'c'};
+        final InputStreamResource mockResource = new InputStreamResource(new ByteArrayInputStream(resourceContents));
+        when(service.buildCodeArchive(anyString(), anyString(), anyString())).thenReturn(mockResource);
+
+        final MockMultipartFile mockFile =
+                new MockMultipartFile("schemaFile", "schemaFile.txt",MediaType.APPLICATION_JSON_VALUE, "{\"dummy\":\"schema\"}".getBytes(StandardCharsets.UTF_8));
+        mockMvc.perform(
+                        multipart("/generators/test-generator")
+                                .file("schema", mockFile.getBytes())
+                                .param("options", "[]")
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_OCTET_STREAM))
+                .andExpect(content().bytes(resourceContents));
     }
 }
