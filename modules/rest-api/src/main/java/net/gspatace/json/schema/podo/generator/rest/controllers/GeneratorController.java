bbe20a111e316aa2248a31a16d00d0b82363de0a
George Spatacean <george.spatacean@gmail.com>
2023-04-02 14:51:11 +0300

    Add Controller specs for Post requests, support for schema as both body payload and file upload; cleanup;

--- modules/rest-api/src/main/java/net/gspatace/json/schema/podo/generator/rest/controllers/GeneratorController.java
+++ modules/rest-api/src/main/java/net/gspatace/json/schema/podo/generator/rest/controllers/GeneratorController.java
@@ -2,7 +2,9 @@ package net.gspatace.json.schema.podo.generator.rest.controllers;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.Explode;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.enums.ParameterStyle;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
@@ -14,6 +16,7 @@ import lombok.extern.slf4j.Slf4j;
 import net.gspatace.json.schema.podo.generator.core.services.GeneratorDescription;
 import net.gspatace.json.schema.podo.generator.core.services.GeneratorNotFoundException;
 import net.gspatace.json.schema.podo.generator.core.services.OptionDescription;
+import net.gspatace.json.schema.podo.generator.rest.adapter.CustomOptionsTransformer;
 import net.gspatace.json.schema.podo.generator.rest.models.ProblemDetails;
 import net.gspatace.json.schema.podo.generator.rest.services.GeneratorsService;
 import org.springframework.core.io.InputStreamResource;
@@ -26,7 +29,9 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * REST Controller over json-schema-podo-generator
@@ -50,6 +55,14 @@ public class GeneratorController {
 
     private final GeneratorsService service;
 
+    private final CustomOptionsTransformer optionsTransformer;
+
+    /**
+     * Endpoint handler for "/".
+     * It returns the list of the available generators
+     *
+     * @return list of available generators
+     */
     @Operation(
             description = "Retrieve all available generators",
             summary = "Retrieve generators",
@@ -73,6 +86,12 @@ public class GeneratorController {
         return service.listGenerators();
     }
 
+    /**
+     * Endpoint handler for /{generatorName}
+     *
+     * @param generatorName the target generator
+     * @return generator details
+     */
     @Operation(
             description = "Retrieve details of a registered generator",
             summary = "Retrieve generator",
@@ -118,6 +137,12 @@ public class GeneratorController {
         return ResponseEntity.ok(service.listGeneratorDescription(generatorName));
     }
 
+    /**
+     * Endpoint handler for "/{generatorName}/options"
+     *
+     * @param generatorName the target generator
+     * @return list of custom specific generator options
+     */
     @Operation(
             summary = "Retrieve generator options",
             description = "Retrieve the list of generator options that can be configured",
@@ -175,31 +200,142 @@ public class GeneratorController {
         return ResponseEntity.ok(service.listGeneratorProperties(generatorName));
     }
 
+    /**
+     * Endpoint handler for "/{generatorName}
+     * It generates an archive with PODOs, based on
+     * input schema and options
+     *
+     * @param generatorName  target generator
+     * @param allQueryParams custom specific generator options
+     * @param schema         the input JSON Schema
+     * @param archiveName    name of the generated code archive
+     * @return the generated code archive
+     * @throws IOException
+     * @throws GeneratorNotFoundException in case the generator was not found
+     */
+    @Operation(
+            summary = "Generate code",
+            description = "Generate code from a JSON Schema input",
+            parameters = {
+                    @Parameter(
+                            name = "generatorName",
+                            in = ParameterIn.PATH,
+                            description = "name of the generator",
+                            example = "java"
+                    ),
+                    @Parameter(
+                            name = "options",
+                            description = "Name (without leading dashes) and value pairs for custom options of this generator",
+                            in = ParameterIn.QUERY,
+                            style = ParameterStyle.FORM,
+                            schema = @Schema(type = "object"),
+                            explode = Explode.TRUE,
+                            example = "{\"package\":\"generated.api\",\"version\":\"1.0.0-SNAPSHOT\"}"
+                    ),
+                    @Parameter(
+                            name = "archiveName",
+                            in = ParameterIn.PATH,
+                            description = "name of the generated archive",
+                            example = "json-schema-generated-podos"
+                    )
+            },
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "JSON Schema",
+                    content = {
+                            @Content(
+                                    examples = {
+                                            @ExampleObject(
+                                                    name = "Class with two fields",
+                                                    description = "This schema defines a \"Product\" object, with 2 fields - " +
+                                                            "\"productId\" as an integer and \"productName\" as a string",
+                                                    summary = "Class with two fields",
+                                                    value = "{\"$schema\":\"https://json-schema.org/draft/2020-12/schema\",\"$id\":\"https://example.com/product.schema.json\",\"title\":\"Product\",\"description\":\"A product from Acme's catalog\",\"type\":\"object\",\"properties\":{\"productId\":{\"description\":\"The unique identifier for a product\",\"type\":\"integer\"},\"productName\":{\"description\":\"Name of the product\",\"type\":\"string\"}}}"
+                                            )
+                                    },
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE
+                            ),
+                            @Content(
+                                    mediaType = MediaType.MULTIPART_FORM_DATA_VALUE
+                            )
+                    }
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "Created",
+                            content = {
+                                    @Content(
+                                            mediaType = MediaType.APPLICATION_OCTET_STREAM_VALUE
+                                    )}
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Generator not found",
+                            content = {
+                                    @Content(
+                                            mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE,
+                                            schema = @Schema(implementation = ProblemDetails.class),
+                                            examples = {
+                                                    @ExampleObject(
+                                                            value = "{\"title\":\"Generator not found.\",\"status\":404,\"details\":\"Generator 'javas' not found\",\"instance\":\"/generators/javas\"}"
+                                                    )}
+                                    )}
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Internal Server Error",
+                            content = {
+                                    @Content(
+                                            mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE,
+                                            schema = @Schema(implementation = ProblemDetails.class),
+                                            examples = {
+                                                    @ExampleObject(
+                                                            value = "{\"title\":\"Custom Properties Object Instantiation error.\",\"status\":500,\"details\":\"Failed to instantiate Custom Properties object of class 'CppSpecificProperties' for generator `cpp`\",\"instance\":\"/generators/cpp/options\"}"
+                                                    )}
+                                    )}
+                    )
+            }
+    )
     @PostMapping(value = "/{generatorName}",
             consumes = {MediaType.MULTIPART_FORM_DATA_VALUE},
             produces = {MediaType.APPLICATION_OCTET_STREAM_VALUE})
-    public ResponseEntity<Resource> serveGeneratedCodeArchive(@PathVariable final String generatorName,
-                                                              @RequestParam final String options,
+    public ResponseEntity<Resource> generateFromMultipartFile(@PathVariable final String generatorName,
+                                                              @RequestParam(required = false) final Map<String, String> allQueryParams,
                                                               @RequestParam final MultipartFile schema,
                                                               @RequestParam(defaultValue = "json-schema-generated-podos") final String archiveName)
             throws IOException, GeneratorNotFoundException {
         final String fileContents = new String(schema.getBytes());
-        return buildResponseEntity(generatorName, options, fileContents, archiveName);
+        final Map<String, String> filteredQueryParams = removeKnownQueryParameters(allQueryParams);
+        return buildResponseEntity(generatorName, optionsTransformer.getCommandOptions(generatorName, filteredQueryParams), fileContents, archiveName);
     }
 
+    /**
+     * Overloaded Endpoint Handler for "/{generatorName}"
+     * It generates an archive with PODOs, based on
+     * input schema and options
+     *
+     * @param generatorName  target generator
+     * @param allQueryParams custom specific generator options
+     * @param schema         the input JSON Schema
+     * @param archiveName    name of the generated code archive
+     * @return the generated code archive
+     * @throws IOException
+     * @throws GeneratorNotFoundException in case the generator was not found
+     */
     @PostMapping(value = "/{generatorName}",
             consumes = {MediaType.APPLICATION_JSON_VALUE},
             produces = {MediaType.APPLICATION_OCTET_STREAM_VALUE})
-    public ResponseEntity<Resource> serveGeneratedCodeArchiveFromString(@PathVariable final String generatorName,
-                                                                        @RequestParam final String options,
-                                                                        @RequestBody final String schema,
-                                                                        @RequestParam(defaultValue = "json-schema-generated-podos") final String archiveName)
+    public ResponseEntity<Resource> generateFromBodyPayload(@PathVariable final String generatorName,
+                                                            @RequestParam(required = false) final Map<String, String> allQueryParams,
+                                                            @RequestBody final String schema,
+                                                            @RequestParam(defaultValue = "json-schema-generated-podos") final String archiveName)
             throws IOException, GeneratorNotFoundException {
-        return buildResponseEntity(generatorName, options, schema, archiveName);
+        final Map<String, String> filteredQueryParams = removeKnownQueryParameters(allQueryParams);
+        return buildResponseEntity(generatorName, optionsTransformer.getCommandOptions(generatorName, filteredQueryParams), schema, archiveName);
     }
 
     private ResponseEntity<Resource> buildResponseEntity(final String generatorName,
-                                                         final String options,
+                                                         final String[] options,
                                                          final String schema,
                                                          final String archiveName)
             throws GeneratorNotFoundException, IOException {
@@ -208,5 +344,20 @@ public class GeneratorController {
                 .contentType(MediaType.APPLICATION_OCTET_STREAM)
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + archiveName + ".zip\"")
                 .body(archive);
+    }
+
+    /**
+     * Parse all passed query parameters and
+     * remove "well-known" parameters
+     *
+     * @param allQueryParams all query parameters of the request
+     * @return query parameters without "well-known" ones
+     */
+    private Map<String, String> removeKnownQueryParameters(final Map<String, String> allQueryParams) {
+        return allQueryParams
+                .entrySet()
+                .stream()
+                .filter(param -> !param.getKey().equals("archiveName"))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 }
